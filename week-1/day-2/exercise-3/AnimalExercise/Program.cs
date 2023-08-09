59be23df96845122ed7c18207ecc273f99537ac3
Vipul <vipulupadhyay563@gmail.com>
2023-08-09 13:32:37 +0530

    Add exercise-3

--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace AnimalExercise
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+            List<Animal> animals = new List<Animal>
+            {
+                new Dog { Name = "Tommy", Age = 5 },
+                new Cat { Name = "Jelly", Age = 2 }
+            };
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}");
+                animal.MakeSound();
+            }
         }
     }
 }
