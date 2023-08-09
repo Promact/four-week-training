a80b565f8f4e4489e122447ab5481eeaddd859e1
Ashish8532 <ashishraj916263@outlook.com>
2023-08-09 16:24:17 +0530

    add exercise 4 for day 2 of week 1

--- week-1/day-2/exercise-4/VehicleManagementSystem/Program.cs
+++ week-1/day-2/exercise-4/VehicleManagementSystem/Program.cs
@@ -1,10 +1,68 @@
 ﻿namespace VehicleManagementSystem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Prompt the user to input a vehicle type
+            Console.WriteLine("1. Factory Design Method.");
+            Console.Write("Enter a vehicle type car or truck: ");
+            string? vehicleType = Console.ReadLine();
+            // Retrieve the appropriate vehicle factory based on user input
+            VehicleFactory vehicleFactory = GetVehicleFactory(vehicleType);
+
+            if (vehicleFactory != null)
+            {
+                // Use the factory to create a vehicle and perform a drive action
+                IVehicle vehicle = vehicleFactory.CreateVehicle();
+                Console.WriteLine("\nVehicle created:");
+                vehicle.Drive();
+            }
+            else
+            {
+                Console.WriteLine("Invalid vehicle type.");
+            }
+
+            Console.WriteLine("\n2.Repository CRUD operation.\n");
+
+            // Create instances of VehicleRepository and VehicleLogger
+            VehicleRepository vehicleRepository = new VehicleRepository();
+            VehicleLogger vehicleLogger = VehicleLogger.Instance;
+
+            // Create a VehicleService and associate it with the repository and logger
+            VehicleService vehicleService = new VehicleService(vehicleRepository, vehicleLogger);
+
+            // Create instances of CarFactory and TruckFactory
+            CarFactory carFactory = new CarFactory();
+            TruckFactory truckFactory = new TruckFactory();
+
+            // List vehicles and demonstrate adding/removing vehicles
+            vehicleService.AddVehicle(carFactory);
+            vehicleService.AddVehicle(carFactory);
+            vehicleService.AddVehicle(truckFactory);
+
+            vehicleService.ListVehicles();
+            vehicleService.RemoveVehicle(1);
+            vehicleService.AddVehicle(truckFactory);
+            vehicleService.ListVehicles();
+
+            // Demonstrate performing actions on vehicles
+            vehicleService.DoSomethingWithVehicle(1);
+            vehicleService.DoSomethingWithVehicle(2);
+        }
+
+        // Helper function to get the appropriate vehicle factory based on input
+        static VehicleFactory GetVehicleFactory(string vehicleType)
+        {
+            switch (vehicleType.ToLower())
+            {
+                case "car":
+                    return new CarFactory();
+                case "truck":
+                    return new TruckFactory();
+                default:
+                    return null;
+            }
         }
     }
 }
---
+++ week-1/day-2/exercise-4/VehicleManagementSystem/VehicleLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleManagementSystem
+{
+    // Define a class for logging vehicle-related messages
+    public class VehicleLogger
+    {
+        // Private constructor to prevent external instantiation
+        private VehicleLogger() { }
+
+        // Create an object for Thread Safety to use double check locking
+        private static readonly object locker = new object();
+
+        // Create a private instance variable for the singleton pattern
+        private static VehicleLogger? instance;
+
+        // Define a public property to access the singleton instance
+        public static VehicleLogger Instance
+        {
+            get
+            {
+                // Thread Safety Singleton using Double-Check Locking
+                if (instance == null)
+                {
+                    lock (locker)
+                    {
+                        // If instance is still null, create a new instance
+                        if (instance == null)
+                        {
+                            instance = new VehicleLogger();
+                        }
+                    }
+                }
+                // Return the singleton instance
+                return instance;
+            }
+        }
+
+        // Method for logging vehicle-related messages
+        public void Log(string message)
+        {
+            Console.WriteLine($"Vehicle Logging: {message}");
+        }
+    }
+}
