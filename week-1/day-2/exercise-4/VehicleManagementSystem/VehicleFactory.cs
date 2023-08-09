474a5435d0e6b2c1847cd4ec9e1c2181a38b2127
Vipul <vipulupadhyay563@gmail.com>
2023-08-09 13:42:22 +0530

    Add exercise-4

---
+++ week-1/day-2/exercise-4/VehicleManagementSystem/VehicleFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleManagementSystem.Interfaces;
+
+namespace VehicleManagementSystem
+{
+    public abstract class VehicleFactory
+    {
+        // abstract method for creating vehicle
+        public abstract IVehicle CreateVehicle();
+
+        public void DoSomethingWithVehicle()
+        {
+            IVehicle vehicle = CreateVehicle();
+            Console.WriteLine("Doing something with the vehicle");
+            // call the drive method on the basis of created vehicle
+            vehicle.Drive();
+        }
+    }
+
+    public class CarFactory : VehicleFactory
+    {
+        // override the Vehiclefactory Method and create a Car instance
+        public override IVehicle CreateVehicle()
+        {
+            return new Car();
+        }
+    }
+
+    public class TruckFactory : VehicleFactory
+    {
+        // override the Vehiclefactory Method and create a Truck instance
+        public override IVehicle CreateVehicle()
+        {
+            return new Truck();
+        }
+    }
+}
