3e8d903785c191d96d888472f8b738af722afb30
pawankhandwe <pawankhandwe@gmail.com>
2023-08-10 15:59:28 +0530

    Day4

--- week-1/day-4/exercise-7/LinqListNumbers/Program.cs
+++ week-1/day-4/exercise-7/LinqListNumbers/Program.cs
@@ -13,6 +13,44 @@
             // 3. Calculate the sum of all numbers
             // 4. Calculate the average of all numbers
             // 5. Find the minimum and maximum values in the list
+
+            List<int> evenNumbers = numbers.Where(num => num % 2 == 0).ToList();
+
+            int specificValue = 20;
+            List<int> greaterValue = numbers.Where(num => num > specificValue).ToList();
+
+            int sum = numbers.Sum();
+
+            double average = numbers.Average();
+
+            int minimum = numbers.Min();
+            int maximum = numbers.Max();
+
+           
+            Console.WriteLine("Even Numbers:");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            foreach (int num in evenNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ResetColor();
+            Console.WriteLine("\nNumbers Greater Than " + specificValue + ":");
+
+            Console.ForegroundColor = ConsoleColor.Blue ;
+            foreach (int num in greaterValue)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\nSum: " + sum);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Average: " + average);
+            Console.ResetColor();
+            Console.WriteLine("Minimum: " + minimum);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Maximum: " + maximum);
+            Console.ResetColor();
         }
     }
 }
--- week-1/day-4/exercise-8/LinqGroupAggregate/Program.cs
+++ week-1/day-4/exercise-8/LinqGroupAggregate/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace LinqGroupAggregate
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
             // Create a list of Product objects
@@ -14,12 +14,63 @@
             new Product { Name = "Jeans", Category = "Clothing", Price = 60.00M },
             new Product { Name = "Sneakers", Category = "Footwear", Price = 100.00M }
         };
-
             // Use LINQ to perform the following operations:
             // 1. Group products by category
             // 2. Count the number of products in each category
             // 3. Calculate the total price of products in each category
             // 4. Find the most expensive product in each category
+
+
+          
+            var productscategory = products.GroupBy(product => product.Category);
+
+            // 2. Count the number of products in each category
+            var productCounts = productscategory.ToDictionary(group => group.Key, group => group.Count());
+
+            // 3. Calculate the total price of products in each category
+            var totalPrices = productscategory.ToDictionary(group => group.Key, group => group.Sum(product => product.Price));
+
+            // 4. Find the most expensive product in each category
+            var expensiveProducts = productscategory.ToDictionary(group => group.Key, group => group.OrderByDescending(product => product.Price).FirstOrDefault());
+
+
+            // Print the results
+            Console.WriteLine("Group products by category:");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            foreach (var group in productscategory)
+            {
+                Console.WriteLine("Category: " + group.Key);
+                foreach (var product in group)
+                {
+                    Console.WriteLine($"  Product: {product.Name}, Price: {product.Price}");
+                }
+            }
+            Console.ResetColor();
+            Console.WriteLine("\nProduct Counts by Category:");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            foreach (var kvp in productCounts)
+            {
+                Console.WriteLine($"Category: {kvp.Key}, Count: {kvp.Value}");
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("\nTotal Prices by Category:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var kvp in totalPrices)
+            {
+                Console.WriteLine($"Category: {kvp.Key}, Total Price: {kvp.Value:C}");
+            }
+            Console.ResetColor();
+            Console.WriteLine("\nMost Expensive Products by Category:");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            foreach (var kvp in expensiveProducts)
+            {
+                Console.WriteLine($"Category: {kvp.Key}, Product: {kvp.Value?.Name}, Price: {kvp.Value?.Price:C}");
+            }
+            Console.ResetColor();
         }
     }
+
 }
