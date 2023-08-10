2133ffd5b74649529e50f3c0580364ab7e65f18b
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-08-10 17:31:00 +0530

    Add exercise 3 day4

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -19,6 +19,41 @@
             //3. Sort people by name
             //4. Project/Select only Name and Country of all people
 
+            //1. Get all people from USA
+            var peopleFromUSA = people.Where(person => person.Country == "USA");
+
+            Console.WriteLine("All People from USA:");
+            foreach (var person in peopleFromUSA)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Country: {person.Country}");
+            }
+
+            // 2. Get all people above 30
+            var peopleAbove30 = people.Where(person => person.Age > 30);
+
+            Console.WriteLine("All People above 30:");
+            foreach (var person in peopleAbove30)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Country: {person.Country}");
+            }
+
+            // 3. Sort people by name
+            var sortedPeopleByName = people.OrderBy(person => person.Name);
+
+            Console.WriteLine("People Sorted by Name:");
+            foreach (var person in sortedPeopleByName)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}, Country: {person.Country}");
+            }
+
+            // 4. Project/Select only Name and Country of all people
+            var projectedPeople = people.Select(person => new { person.Name, person.Country });
+
+            Console.WriteLine("Projected List (Name and Country):");
+            foreach (var item in projectedPeople)
+            {
+                Console.WriteLine($"Name: {item.Name}, Country: {item.Country}");
+            }
         }
     }
 
