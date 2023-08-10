3f34b43eb07fcb90223d020dd0f2693c93e5bc12
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-08-10 17:55:10 +0530

    Add exercise 6 day4

--- week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
+++ week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqFilterAndSort
+﻿using System;
+
+namespace LinqFilterAndSort
 {
     internal class Program
     {
@@ -7,6 +9,38 @@
             // Create a list of Person objects
             // Use LINQ to filter and sort the list
             // Print the filtered and sorted list of people to the console
+            List<Person> people = new List<Person>
+            {
+            new Person { FirstName = "Sakshi", LastName = "Kachhi", Age = 30 },
+            new Person { FirstName = "Riya", LastName = "Sharma", Age = 33 },
+            new Person { FirstName = "Aisha", LastName = "Singh", Age = 18 },
+            new Person { FirstName = "Janvi", LastName = "Thakkar", Age = 21 },
+            new Person { FirstName = "Parth", LastName = "Parmar", Age = 25 },
+            new Person { FirstName = "Henny", LastName = "Patel", Age = 14 }
+        };
+
+            // Use LINQ to filter the list to people who are 18 years old or older
+            var filteredList = people.Where(person => person.Age >= 18).ToList();
+
+            // Use LINQ to sort the filtered list by last name, then by first name
+            var sortedList = filteredList.OrderBy(person => person.LastName).ThenBy(person => person.FirstName).ToList();
+
+            // Print the filtered and sorted list to the console
+            Console.WriteLine("Filtered and Sorted List:");
+
+
+            foreach (var person in sortedList)
+            {
+                Console.WriteLine($"First Name: {person.FirstName}, Last Name: {person.LastName}, Age: {person.Age}");
+            }
         }
+
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
