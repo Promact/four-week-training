a44ae246ca4c2b01e7cca9b2b9dab2e6c488f660
Vipul <vipulupadhyay563@gmail.com>
2023-08-10 13:13:15 +0530

    Add day-4 exercise-2

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace IteratorsApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -14,7 +14,20 @@
         // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
         public static IEnumerable<int> FibonacciSequence()
         {
-            throw new NotImplementedException();
+            int first = 0;
+            int second = 1;
+
+            yield return first;
+            yield return second;
+
+            // Generating the first 10 Fibonacci numbers
+            for (int i = 2; i < 10; i++)
+            {
+                int next = first + second;
+                yield return next;
+                first = second;
+                second = next;
+            }
         }
     }
 }
