b0da768d92165241d28e3b6d05b44e7336ddb15f
Ashish8532 <ashishraj916263@outlook.com>
2023-08-11 16:11:11 +0530

    add exercise 3 for day 5 of week 1

--- week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
+++ week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
@@ -1,20 +1,38 @@
-﻿namespace AsyncAwaitBasics
+﻿using System.Diagnostics;
+
+namespace AsyncAwaitBasics
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             // Call PerformCalculations and measure time taken using Stopwatch
+            Stopwatch sw = Stopwatch.StartNew();
+
+            await PerformCalculations(3);
+
+            sw.Stop();
+            Console.WriteLine("Total Time taken: {0} ms", sw.ElapsedMilliseconds);
         }
 
         static async Task SimulateLongRunningTask(int delayInSeconds)
         {
             // Implement long-running task simulation
+            await Task.Delay(TimeSpan.FromSeconds(delayInSeconds));
         }
 
         static async Task PerformCalculations(int numberOfTasks)
         {
             // Start long-running tasks concurrently and wait for them to complete
+            Task[] task = new Task[numberOfTasks];
+
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                task[i] = SimulateLongRunningTask(i + 1);
+            }
+            await Task.WhenAll(task);
+
+            Console.WriteLine("Task Completed.");
         }
     }
 }
