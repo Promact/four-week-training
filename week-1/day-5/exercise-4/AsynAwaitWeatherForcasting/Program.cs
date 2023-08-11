688d880b5c70d635ea1e87acca2f5f6062cc7b29
hiren979 <hirenchavda141@gmail.com>
2023-08-11 17:28:32 +0530

    update

--- week-1/day-5/exercise-4/AsynAwaitWeatherForcasting/Program.cs
+++ week-1/day-5/exercise-4/AsynAwaitWeatherForcasting/Program.cs
@@ -1,45 +1,63 @@
-﻿namespace Day5_Task4
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Day5_Task4
 {
     internal class Program
     {
-        static  void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            String[] urls =
-            {
-                "www.google.com",
-                "www.youtube.com",
-            };
+            String urls =
+                "https://api.openweathermap.org/data/2.5/weather?lat=22.3072&lon=73.1812&appid=cfc445455912c235199387bb8f8d45c6";
 
-            var tasks = new Task<string>[urls.Length];
-            for(int i = 0;i< urls.Length;i++)
-            {
-                tasks[i] = fetchDataAsync(urls[i]);
-            }
 
-             Task.WhenAll(tasks).GetAwaiter().GetResult();
+            WeatherData weatherData = await fetchDataAsync(urls);
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
-            for (int i = 0; i < tasks.Length; i++)
-            {
-                Console.WriteLine($"URL: {urls[i]}, Content Length: {tasks[i].Result.Length}");
-            }
+
+            Console.WriteLine($"Weather in vadodara:");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Temperature: " + (int)(weatherData.Main.Temp - 273.15) + "°C");
+            Console.WriteLine($"Weather: {weatherData.Weather[0].Description}");
+            Console.ResetColor();
 
         }
 
-        static async Task<String> fetchDataAsync(string url)
+        //for fetching data from api
+        static async Task<WeatherData> fetchDataAsync(string url)
         {
             using(HttpClient client = new HttpClient())
             {
-                try
-                {
-                    String content = await client.GetStringAsync(url);
-                    return content;
-                }
-                catch (Exception ex)
-                {
-                    return $"Error fetching {url}: {ex.Message}";
+                HttpResponseMessage response = await client.GetAsync(url);
 
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(json);
+                    return weatherData;
+                }
+                else
+                {
+                    throw new Exception($"{response.StatusCode}");
                 }
             }
+        }
+
+        public class WeatherData
+        {
+            public MainData Main { get; set; }
+            public WeatherDesc[] Weather { get; set; }
+        }
+
+        public class MainData
+        {
+            public float Temp { get; set; }
+        }
+
+        public class WeatherDataDesc
+        {
+            public String Description { get; set; }
         }
 
     }
