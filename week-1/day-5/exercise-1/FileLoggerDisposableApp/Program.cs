f97f3274ed3417ee248df2983f04bc0bd771748e
divyanshsahu1606 <divyanshsahu00010@gmail.com>
2023-08-11 16:29:32 +0530

    Day-5

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -1,30 +1,64 @@
-﻿namespace FileLoggerDisposableApp
+﻿using System.ComponentModel;
+
+namespace FileLoggerDisposableApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            using (var logger = new FileLogger("C:\\Users\\admin\\log.txt"))
+            {
+                // More log entries...
+                logger.Log("This is a log message.");
+                logger.Log("Another log message.");
+               
+            }
+
         }
     }
 
     class FileLogger : IDisposable
     {
         private StreamWriter _writer;
+        private bool _disposed = false;
 
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath, append: true);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
         }
 
         public void Log(string message)
         {
-            // Write message to the file
+                // Write message to the file
+                if (_disposed)
+                {
+                    throw new ObjectDisposedException("FileLogger", "Cannot log to a disposed FileLogger.");
+                }
+
+                _writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}");
+                _writer.Flush();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _writer.Dispose();
+                }
+
+                _disposed = true;
+            }
         }
     }
 }
--- week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
+++ week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
@@ -1,20 +1,45 @@
-﻿namespace AsyncAwaitBasics
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitBasics
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             // Call PerformCalculations and measure time taken using Stopwatch
+            int numberOfTasks = 5; // You can change this value as needed
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            await PerformCalculations(numberOfTasks);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"All tasks completed in {stopwatch.Elapsed.TotalSeconds} seconds.");
+        }
+
+        private static async Task PerformCalculations(int numberOfTasks)
+        {
+            // Start long-running tasks concurrently and wait for them to complete
+            var tasks = new Task[numberOfTasks];
+
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                tasks[i] = SimulateLongRunningTask(i + 1); // Pass i + 1 as the delayInSeconds
+            }
+
+            await Task.WhenAll(tasks);
         }
 
         static async Task SimulateLongRunningTask(int delayInSeconds)
         {
             // Implement long-running task simulation
-        }
-
-        static async Task PerformCalculations(int numberOfTasks)
-        {
-            // Start long-running tasks concurrently and wait for them to complete
+            Console.WriteLine($"Task with {delayInSeconds} second delay started.");
+            await Task.Delay(TimeSpan.FromSeconds(delayInSeconds));
+            Console.WriteLine($"Task with {delayInSeconds} second delay completed.");
         }
     }
 }
+
