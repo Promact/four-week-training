362b475b5671ecb414d002e97364123f5106d04e
Prakhar <agrawalprakhar893@gmail.com>
2023-08-15 19:46:08 +0530

    day2

--- week-1/day-2/exercise-1/Circle/Circle.cs
+++ week-1/day-2/exercise-1/Circle/Circle.cs
@@ -9,5 +9,21 @@ namespace Circle
     internal class Circle
     {
         // Implement the Circle class here
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
     }
 }
--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             // Create a Circle object and display its area and circumference
+            Console.Write("Enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Circle circle = new Circle(radius);
+
+            double area = circle.CalculateArea();
+            double circumference = circle.CalculateCircumference();
+
+            Console.WriteLine($"Circle Area: {area}");
+            Console.WriteLine($"Circle Circumference: {circumference}");
+
         }
     }
 }
