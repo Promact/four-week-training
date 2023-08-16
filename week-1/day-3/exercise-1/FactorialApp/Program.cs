c6446d520662ed3f0aeaf31c93904992a88a86cc
Prakhar <agrawalprakhar893@gmail.com>
2023-08-16 15:29:12 +0530

    Day 3 Tasks

--- week-1/day-3/exercise-1/FactorialApp/Program.cs
+++ week-1/day-3/exercise-1/FactorialApp/Program.cs
@@ -12,9 +12,20 @@
             Console.WriteLine($"The factorial of {number} is: {factorial}");
         }
 
-        public static long CalculateFactorial(int number)
+        public static long CalculateFactorial(int n )
         {
-            throw new NotImplementedException();
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
         }
     }
 }
