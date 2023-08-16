7d489f1b3a232f559e34c27b6810fdb32272910b
Prakhar <agrawalprakhar893@gmail.com>
2023-08-16 14:17:03 +0530

    Day 2 Tasks

--- week-1/day-2/exercise-1/Circle/Circle.cs
+++ week-1/day-2/exercise-1/Circle/Circle.cs
@@ -9,5 +9,22 @@ namespace Circle
     internal class Circle
     {
         // Implement the Circle class here
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
     }
 }
