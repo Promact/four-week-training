41318d36c9b79b0553bf37fd71e07fe3e3af9e46
Prakhar <agrawalprakhar893@gmail.com>
2023-08-17 11:03:57 +0530

    Day 5

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            // Use FileLogger and dispose of it properly
+            // Use FileLogger and dispose of it properly// 
+            using (FileLogger logger = new FileLogger("log.txt"))
+            {
+                logger.Log("This is a log message.");
+                logger.Log("Another log message.");
+                // ... more logging
+
+            } // logger will be automatically disposed here
         }
     }
 
@@ -15,16 +22,31 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath, true); // 'true' appends to the file if it already exists
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_writer != null)
+                {
+                    _writer.Close();
+                    _writer.Dispose();
+                }
+            }
+        }
         public void Log(string message)
         {
             // Write message to the file
+            _writer.WriteLine($"{DateTime.Now}: {message}");
         }
     }
 }
--- week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
+++ week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
@@ -1,20 +1,42 @@
-﻿namespace AsyncAwaitBasics
+﻿using System.Diagnostics;
+
+namespace AsyncAwaitBasics
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             // Call PerformCalculations and measure time taken using Stopwatch
+            await MeasureTimeTakenAsync();
         }
 
+        static async Task MeasureTimeTakenAsync()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            int numberOfTasks = 5;
+            await PerformCalculations(numberOfTasks);
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+        }
         static async Task SimulateLongRunningTask(int delayInSeconds)
         {
             // Implement long-running task simulation
+            await Task.Delay(TimeSpan.FromSeconds(delayInSeconds));
         }
 
         static async Task PerformCalculations(int numberOfTasks)
         {
-            // Start long-running tasks concurrently and wait for them to complete
+            // Start long-running tasks concurrently and wait for them to
+            // 
+            Task[] tasks = new Task[numberOfTasks];
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                tasks[i] = SimulateLongRunningTask(i + 1);
+            }
+
+            await Task.WhenAll(tasks);
         }
     }
 }
